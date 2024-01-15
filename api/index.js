3efd1156218d4c7d247b30b879f0cf20c47df07b
Aynansh <96484069+Aynansh@users.noreply.github.com>
2024-01-15 17:44:23 +0530

    Update index.js 2

--- api/index.js
+++ api/index.js
@@ -17,15 +17,11 @@ const post = require("./models/Post.js");
 const uploadmiddleware = multer({ dest: "uploads/" });
 const upload = multer();
 
-app.use(cors());
-app.all("*", function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
+app.use(cors({
+  origin: 'http://localhost:3000/',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieparser());
